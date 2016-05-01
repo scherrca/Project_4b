8675888df5eab517d744763bab753964a9d243d5
Caito Scherr <scherr.ca@gmail.com>
2016-05-01 13:21:04 -0700

    test 4 succeeded, still one error to resolve: test 5

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,8 +26,5 @@
     <footer>
         <p>&copy; Caito {{ date('Y') }}</p>
     </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
 </body>
 </html>
