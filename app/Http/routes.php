d7d41f2467832d9765860671192a202bce355c46
Caito Scherr <scherr.ca@gmail.com>
2016-05-05 19:07:39 -0700

    removed all routes from being nested under the middleware route- same issue as with P3: not deploying

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,27 +1,32 @@
 <?php
 
-   	//Route to the index page
+
+    //Route to the index page
     Route::get('/', 'P4Controller@getIndex');
 
     //Route to creating a new company
-    Route::get('/create', 'P4Controller@getCreate');
+    Route::get('/company/create', 'P4Controller@getCreate');
     //Route to the post data for creating a company
-    Route::post('/create', 'P4Controller@postCreate');
+    Route::post('/company/create', 'P4Controller@postCreate');
 
     //Route to updating info for a company
-    Route::get('/update', 'P4Controller@getUpdate');
+    Route::get('/company/update', 'P4Controller@getUpdate');
     //Route to the post data for updating info for a company
-    Route::post('/update', 'P4Controller@postUpdate');
+    Route::post('/company/update', 'P4Controller@postUpdate');
 
     //Route to deleting a company
-    Route::get('/delete', 'P4Controller@getDelete');
+    Route::get('/company/delete', 'P4Controller@getDelete');
     //Route to the post data for deleting a company
-    Route::post('/delete', 'P4Controller@postDelete');
+    Route::post('/company/delete', 'P4Controller@postDelete');
 
     //Route to deleting a company
-    Route::get('/show', 'P4Controller@getShow');
+    Route::get('/company/show', 'P4Controller@getShow');
     //Route to the post data for deleting a company
-    Route::post('/show', 'P4Controller@postShow');
+    Route::post('/company/show', 'P4Controller@postShow');
+
+Route::group(['middleware' => ['web']], function () {
+
+});
 
 
 
--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -2,7 +2,35 @@
 
 @section('content')
     <h2>Add a Company</h2>
-    <p>Placeholder</p>
-    <br><br>
+    <form method='POST' action='/create'>
+        {{ csrf_field() }}
+        <br><br>
+        <label>Date (of most recent activity):</label>
+        <br>
+        <input type="text" id="contact" name="contact">
+        <br><br>
+        <label>Name of Company: </label>
+        <br>
+        <input type="text" id="name" name="name">
+        <br><br>
+        <label>Action (i.e. "submitted resume"): </label>
+        <br>
+        <input type="text" id="contact" name="contact">
+        <br><br>
+        <label>Contact Name: </label>
+        <br>
+        <input type="text" id="contact" name="contact">
+        <br><br>
+        <label>Rating: </label>
+        <br>
+        <input type="text" id="rating" name="rating">
+        <br><br>
+        <label>Notes: </label>
+        <br>
+        <input type="text" id="notes" name="notes">
+        <br><br><br>
+        <button type="submit">Submit</button>
+        <br><br>
+    </form>
     <br>
 @stop  
